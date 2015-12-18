371107a7cf9c413907e6a43366119896a41e191e
Shildon <shildondu@gmail.com>
2015-12-18 21:46:54 +0800

    file transfer

---
+++ src/main/java/com/shildon/chowder/transfer/FileTransfer.java
@@ -0,0 +1,94 @@
+package com.shildon.chowder.transfer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 文件传输工具。
+ * @author shildon<shildondu@gmail.com>
+ * @date Dec 16, 2015 10:39:01 PM
+ *
+ */
+public class FileTransfer {
+	
+	/**
+	 * 设置http下载相应头。
+	 * @param response
+	 */
+	private void setHttpDownloadHead(HttpServletResponse response, String fileName) {
+		try {
+			//转换成IOS8859-1字符以供浏览器显示
+			fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		response.setContentType("application/octet-stream");
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+	}
+	
+	/**
+	 * 将文件写入到response的流。
+	 * @param response
+	 * @param prefix 路径前缀，以/结尾
+	 * @param fileName 文件名
+	 */
+	public void download(HttpServletResponse response, String prefix, String fileName) {
+		setHttpDownloadHead(response, fileName);
+		try {
+			download(response.getOutputStream(), prefix + fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 将文件写入流。
+	 * @param outputStream
+	 * @param fullPath
+	 */
+	public void download(OutputStream outputStream, String fullPath) {
+		// Path类似与java.io.File，但封装了更多的对路径的操作。
+		Path path = Paths.get(fullPath);
+		// Files封装了许多对Path的操作。
+		if (Files.exists(path)) {
+			try {
+				Files.copy(path, outputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 将文件写入目标路径。
+	 * @param targetPath
+	 * @param fullPath
+	 */
+	public void download(String targetPath, String fullPath) {
+		Path target = Paths.get(targetPath);
+		Path source = Paths.get(fullPath);
+		
+		if (Files.exists(source)) {
+			if (Files.exists(target)) {
+				try {
+					Files.createFile(target);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				Files.copy(source, target);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/test/java/transfer/FileTransferTest.java
@@ -0,0 +1,37 @@
+package transfer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.shildon.chowder.transfer.FileTransfer;
+
+public class FileTransferTest {
+	
+	private FileTransfer fileTransfer;
+	
+	@Before
+	public void init() {
+		fileTransfer = new FileTransfer();
+	}
+	
+	@Test
+	public void test() {
+		String fullPath = "/home/shildon/Downloads/test.txt";
+		File file = new File("/home/shildon/Downloads/newTest.txt");
+		try (FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+
+			fileTransfer.download(fileOutputStream, fullPath);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
